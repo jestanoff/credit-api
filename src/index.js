3f3a08eb1fee84f4b01f8b7a7a176e2b68956f56
Shockwave <me@showckwaves-mbp.home>
2018-11-18 23:17:16 +0000

    Adds helmet middleware to secure http headers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 
 const express = require('express')
 const Joi = require('joi'); // use Joi to validate req object on POST/PUT requests
-const app = express()
+const helmet = require('helmet')
 const authentication = require('./middlewares/authentication')
+const app = express()
 
 const port = process.env.port || 3000
 
 // Middlewares
-app.use(express.json()); // middleware so res body is processed as JSON
+app.use(helmet()); // Helps secure the app by setting various http headers
+app.use(express.json()); // Process res body as JSON
 app.use(authentication)
 
 app.get('/', (req, res) => res.send('Car Wash API!'))
