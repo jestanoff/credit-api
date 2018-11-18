822f4922dc465b89ee134e5c40fb463fd54720c6
Shockwave <me@showckwaves-mbp.home>
2018-11-18 22:32:27 +0000

    Adds nodemon, joi and json middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+
 const express = require('express')
+const Joi = require('joi'); // use Joi to validate req object on POST/PUT requests
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
+
+app.use(express.json()); // middleware so res body is processed as JSON
 
 app.get('/', (req, res) => res.send('Car Wash API!'))
+
+app.get('/api/ids', (req, res) => res.send('Hello'))
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
