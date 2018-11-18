9960ad08448935b842d1309a825dee626ed87702
Shockwave <me@showckwaves-mbp.home>
2018-11-18 23:21:50 +0000

    Adds morgan middleware to log requests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const morgan = require('morgan')
 const Joi = require('joi'); // use Joi to validate req object on POST/PUT requests
 const helmet = require('helmet')
 const authentication = require('./middlewares/authentication')
@@ -10,6 +11,7 @@ const port = process.env.port || 3000
 // Middlewares
 app.use(helmet()); // Helps secure the app by setting various http headers
 app.use(express.json()); // Process res body as JSON
+app.use(morgan()); // Logging requests
 app.use(authentication)
 
 app.get('/', (req, res) => res.send('Car Wash API!'))
