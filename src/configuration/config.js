f4c4fabfafe3b4f2f5fc17a43bf7608b47a65d58
Stefan Dzhestanov <stefan.dzhestanov@waitrose.co.uk>
2019-08-21 16:09:13 +0100

    Adds jwt authentication

---
+++ src/configuration/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: '_@4eB59s9h!nJWm4tzF.Zve@9MXZ35sBjAYRcs2P!VDcR',
+};
--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const Joi = require('joi'); // use Joi to validate req object on POST/PUT requests
-const helmet = require('helmet')
-const authentication = require('./middlewares/authentication')
-const app = express()
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const authentication = require('./middlewares/authentication');
+const config = require('./configuration/config');
+
+const app = express();
 
 // Database
 mongoose.connect('mongodb://localhost/carwash', { useNewUrlParser: true })
     .then(() => console.log('connected to MongoDB'))
-    .catch(err => console.log('could not connect to MongoDB', err))
+    .catch(err => console.log('could not connect to MongoDB', err));
 
 const Transaction = mongoose.model('Transaction', new mongoose.Schema({
     currentBalance: Number,
     date: { type: Date, default: Date.now },
     value: Number,
-}))
+}));
 const transaction = new Transaction({
     currentBalance: 0,
     value: 1,
-})
+});
 
 const createTransaction = async () => {
     const result = await transaction.save();
-    console.log(result)
-}
+    console.log(result);
+};
 // createTransaction();
 
 const getTransactions = async () => {
@@ -48,30 +52,58 @@ const getTransactions = async () => {
         .sort({ value: 1 })
         .select({ date: 1, value: 1 })
     console.log(result)
-}
+};
 getTransactions();
 
 const port = process.env.port || 3000
 
-// Middlewares
-app.use(helmet()); // Helps secure the app by setting various http headers
-app.use(express.json()); // Process res body as JSON
-app.use(morgan('dev')); // Logging requests
-app.get('/', (req, res) => res.send('<h1>Car Wash API</h1>'))
+// MIDDLEWARES
 
-app.use(authentication)
+// Set secret
+app.set('Secret', config.secret);
+
+// Helps secure the app by setting various http headers
+app.use(helmet());
+
+// Use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Process res body as JSON
+app.use(express.json());
+
+
+app.get('/', (req, res) => res.send('<h1>Car Wash API</h1>'));
+
+
+app.post('/api/token', (req, res) => {
+  console.log('Token', req.body);
+  if (req.body.username === 'admin') {
+    if (req.body.password === '12345') {
+      const payload = { check: true };
+      const token = jwt.sign(payload, app.get('Secret'), { expiresIn: 1440 }); // expires in 24h
+
+      res.json({ message: 'Auth done', token });
+    } 
+  } else {
+    res.json({ message: 'Username or password not found' });
+  }
+});
+// app.use(authentication);
 
 
 app.get('/api/cards', (req, res) => {
   res.send({
     cards: [],
   });
-})
+});
 
 app.post('/api/cards', (req, res) => {
   res.send({
     cards: [],
   });
-})
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}`))
+app.listen(port, () => console.log(`App listening on port ${port}`));
