bdf07f7468c4acc12f4c10ba4ee6b84ee9116d26
Stefan Dzhestanov <stefan.dzhestanov@waitrose.co.uk>
2019-09-01 13:15:34 +0100

    Removes unused transaction code

--- src/models/card.js
+++ src/models/card.js
@@ -46,24 +46,3 @@ export const getCards = (limit = 50) => Card.find().limit(limit);
 export const createCard = () => new Card({}).save();
 
 export const getCard = id => Card.find({ _id: id });
-
-export const getTransactions = async () => {
-  /* COMPARISON */
-  // eq (equal)
-  // ne (not equal)
-  // gt (greater than)
-  // gte (greater than or equal to)
-  // lt (less than)
-  // lte (less than or equal to)
-  // in (one of array of values)
-  // nin (not in)
-  /* LOGICAL */
-  // or
-  // and
-  await Card.find({ balance: { $gte: 0, $lte: 1 } })
-    .or([{ balance: 1 }, { balance: 10 }])
-    .limit(20)
-    .sort({ balance: 1 })
-    .select({ date: 1, balance: 1 });
-};
-getTransactions();
