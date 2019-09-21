ac85f5d0d066ffabdb922ed1957c6067c45858aa
Stefan Dzhestanov <stefan.dzhestanov@waitrose.co.uk>
2019-09-21 09:06:22 +0100

    Adds unit test coverage reporters

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  // automock: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['**/src/server/**/*.js', '!**/node_modules/**'],
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['lcov', 'text-summary'],
   modulePaths: ['<rootDir>/src/'],
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/'],
