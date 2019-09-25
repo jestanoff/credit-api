0afaaee99f8c32ef9d0f1e4463792bc6f6e085b6
Stefan Dzhestanov <stefan.dzhestanov@waitrose.co.uk>
2019-09-26 00:21:44 +0100

    Adds a parse that slices the data to 12 bytes

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import SerialPort from 'serialport';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import MockBinding from '@serialport/binding-mock';
+import ByteLength from '@serialport/parser-byte-length';
 
 export default () => {
   SerialPort.Binding = MockBinding;
@@ -10,17 +11,16 @@ export default () => {
     if (err) console.log(err);
     console.log(`Listening on serial port ${port.path}`);
   });
+  const parser = port.pipe(new ByteLength({ length: 12 }));
+
 
   port.write([171, 86, 1, 1, 123, 175, 31, 104, 1, 14, 217, 192], err => {
     if (err) console.log('Error on write: ', err.message);
   });
-
-  setTimeout(() => {
-    port.write(Buffer.from(`AB5601017BAF1F68${'010E'}D9C0`, 'hex'));
-  }, 0);
+  port.write(Buffer.from(`AB5601017BAF1F68${'010E'}D9C0`, 'hex'));
 
   // Switches the port into "flowing mode"
-  port.on('data', data => {
+  parser.on('data', data => {
     console.log('Data:', data);
   });
 };
